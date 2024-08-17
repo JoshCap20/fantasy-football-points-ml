7aaebdf0e3098fc238f43c8e436032f788a2ec05
Josh Caponigro <joshcap@email.unc.edu>
2024-08-17 02:21:35 -0400

    Logging tweaks

--- main.py
+++ main.py
@@ -4,26 +4,29 @@ from model_training import train_model
 from evaluation import save_results, calculate_fantasy_data_rmse
 import logging
 
+logger = logging.getLogger(__name__)
 
 def main():
     path = "data/"
     train_df = load_and_process_data(path + "aggregated_2015.csv")
     test_df = load_and_process_data(path + "aggregated_2016.csv")
-
+    logger.info("Data loaded and processed successfully")
+    
     train_df, features = create_features(train_df)
     test_df, _ = create_features(test_df)
+    logger.info("Features created successfully")
 
     positions = train_df["Pos"].unique()
     results = {}
 
     for position in positions:
-        logging.info(f"Learning for Position {position} ...")
+        logger.info(f"Learning for Position {position} ...")
         results[position] = train_model(train_df, test_df, features, position)
 
     save_results(results, "rmse.csv")
     calculate_fantasy_data_rmse(test_df)
-    logging.info("Results saved to rmse.csv and FantasyData_rmse.csv")
-    logging.info("Program finished normally")
+    logger.info("Results saved to rmse.csv and FantasyData_rmse.csv")
+    logger.info("Program finished normally")
 
 
 if __name__ == "__main__":
