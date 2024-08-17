cd15f19bc1bfa8b8c1c4f65923cf34af21184c9c
Josh Caponigro <joshcap@email.unc.edu>
2024-08-17 02:31:55 -0400

    Remove unused files for now

--- model_training.py
+++ model_training.py
@@ -1,10 +1,11 @@
 from sklearn.discriminant_analysis import StandardScaler
 from sklearn.model_selection import GridSearchCV, cross_val_score
-from sklearn.linear_model import RidgeCV, ElasticNetCV
+from sklearn.linear_model import RidgeCV, ElasticNetCV, BayesianRidge
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
-from sklearn.impute import SimpleImputer
 from sklearn.pipeline import make_pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.svm import SVC
 import pandas as pd
 import numpy as np
 
@@ -38,7 +39,7 @@ def train_model(
         ),
         "GradientBoostingRegressor": GridSearchCV(
             GradientBoostingRegressor(max_depth=3),
-            param_grid={"n_estimators": [50], "learning_rate": [0.1]},
+            param_grid={"n_estimators": [50], "learning_rate": [0.3]},
             cv=5,
         ),
     }
@@ -76,3 +77,38 @@ def train_model(
         }
 
     return results
+
+    # if(est == "GradientBoostingRegressor"):
+    #     n_estimators = [50]
+    #     learning_rate = [0.1]
+    #     param_grid = {'n_estimators': n_estimators, 'learning_rate': learning_rate}
+    #     grid_search = GridSearchCV(GradientBoostingRegressor(max_depth=3), param_grid, cv=5)
+    #     grid_search.fit(df_pos_train[features], df_pos_train['FD points'])
+
+    # elif(est == "RandomForestRegressor"):
+    #     n_estimators = [50]
+    #     param_grid = {'n_estimators': n_estimators}
+    #     grid_search = GridSearchCV(RandomForestRegressor(max_depth=3), param_grid, cv=5)
+    #     grid_search.fit(df_pos_train[features], df_pos_train['FD points'])
+
+    # elif(est == "ElasticNet"):
+    #     grid_search = ElasticNetCV().fit(df_pos_train[features], df_pos_train['FD points'])
+
+    # elif(est == "BayesianRidge"):
+    #     alpha_1 = [1e-6, 1e-5, 1e-7]
+    #     alpha_2 = [1e-6, 1e-5, 1e-7]
+    #     lambda_1 = [1e-6, 1e-5, 1e-7]
+    #     lambda_2 = [1e-6, 1e-5, 1e-7]
+    #     param_grid = {'alpha_1': alpha_1, 'alpha_2':alpha_2, 'lambda_1':lambda_1, 'lambda_2':lambda_2}
+    #     grid_search = GridSearchCV(BayesianRidge(), param_grid, cv=5)
+    #     grid_search.fit(df_pos_train[features], df_pos_train[target])
+
+    # elif(est == "Ridge"):
+    #     grid_search = RidgeCV().fit(df_pos_train[features], df_pos_train['FD points'])
+
+    # elif(est == "SVM"):
+    #     C = [50]
+    #     gamma = [0.3]
+    #     param_grid = {'C': C, 'gamma': gamma}
+    #     grid_search = GridSearchCV(SVC(), param_grid, cv=5)
+    #     grid_search.fit(df_pos_train[features], df_pos_train['FD points'])
