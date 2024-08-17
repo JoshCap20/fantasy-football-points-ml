a0a9dfa894eb30a10dce12c58f10f527fe5a17d6
Josh Caponigro <joshcap@email.unc.edu>
2024-08-17 02:39:49 -0400

    Refactor model outputs

--- evaluation.py
+++ evaluation.py
@@ -1,13 +1,24 @@
 import pandas as pd
 
 
-def save_results(results: dict[str, dict[str, float]], filename: str) -> None:
-    df_rmse = pd.DataFrame(results).T
-    df_rmse.to_csv(f"results/{filename}", header=True, index=True)
-
-
 def calculate_fantasy_data_rmse(df: pd.DataFrame) -> pd.Series:
     df["diff"] = (df["proj"] - df["FD points"]) ** 2.0
     FantasyData_rmse = df.groupby(["Pos"])["diff"].mean() ** 0.5
-    save_results(FantasyData_rmse, "FantasyData_rmse.csv")
     return FantasyData_rmse
+
+
+class OutputManager:
+    OUTPUT_DIRECTORY = "results/"
+
+    @classmethod
+    def save_results_from_dictionary(
+        cls, results: dict[str, dict[str, float]], filename: str
+    ) -> None:
+        df_rmse = pd.DataFrame(results).T
+        cls._save_dataframe(df_rmse, filename)
+
+    @classmethod
+    def save_results_from_dataframe(
+        cls, df: pd.DataFrame, filename: str, header: bool = True, index: bool = True
+    ) -> None:
+        df.to_csv(f"{cls.OUTPUT_DIRECTORY}/{filename}", header=header, index=index)
