019f4d57c12a81cdb12851e5109a808efc8ea2d8
Josh Caponigro <joshcap@email.unc.edu>
2024-08-17 02:44:24 -0400

    Refactor logger for filenames

--- logger.py
+++ logger.py
@@ -2,15 +2,18 @@
 Logger module for the project
 """
 
-import logging
+def get_logger(name: str):
+    import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
 
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
 
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-console_handler.setFormatter(formatter)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    console_handler.setFormatter(formatter)
 
-logger.addHandler(console_handler)
+    logger.addHandler(console_handler)
+
+    return logger
