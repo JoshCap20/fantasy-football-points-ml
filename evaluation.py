5c019382170ca9fae81cc91b133fc653f62acd05
Josh Caponigro <joshcap@email.unc.edu>
2024-08-17 02:21:53 -0400

    Minor tweaks

--- evaluation.py
+++ evaluation.py
@@ -6,8 +6,8 @@ def save_results(results: dict[str, dict[str, float]], filename: str) -> None:
     df_rmse.to_csv(filename, header=True, index=True)
 
 
-def calculate_fantasy_data_rmse(test_df: pd.DataFrame) -> pd.Series:
-    test_df["diff"] = (test_df["proj"] - test_df["FD points"]) ** 2.0
-    FantasyData_rmse = test_df.groupby(["Pos"])["diff"].mean() ** 0.5
+def calculate_fantasy_data_rmse(df: pd.DataFrame) -> pd.Series:
+    df["diff"] = (df["proj"] - df["FD points"]) ** 2.0
+    FantasyData_rmse = df.groupby(["Pos"])["diff"].mean() ** 0.5
     FantasyData_rmse.to_csv("FantasyData_rmse.csv", header=True, index=True)
     return FantasyData_rmse
