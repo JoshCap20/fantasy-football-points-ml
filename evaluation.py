3013da7efcaa761400124d2121f2b12905d6e4d1
Josh Caponigro <joshcap@email.unc.edu>
2024-08-17 02:28:27 -0400

    Output data to results/

--- evaluation.py
+++ evaluation.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 def save_results(results: dict[str, dict[str, float]], filename: str) -> None:
     df_rmse = pd.DataFrame(results).T
-    df_rmse.to_csv(filename, header=True, index=True)
+    df_rmse.to_csv(f"results/{filename}", header=True, index=True)
 
 
 def calculate_fantasy_data_rmse(df: pd.DataFrame) -> pd.Series:
     df["diff"] = (df["proj"] - df["FD points"]) ** 2.0
     FantasyData_rmse = df.groupby(["Pos"])["diff"].mean() ** 0.5
-    FantasyData_rmse.to_csv("FantasyData_rmse.csv", header=True, index=True)
+    save_results(FantasyData_rmse, "FantasyData_rmse.csv")
     return FantasyData_rmse
