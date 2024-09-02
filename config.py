308e8060ced06d670e113bda0ab6053366dd041d
Josh Caponigro <joshcap@email.unc.edu>
2024-09-01 20:58:26 -0400

    Change debug

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-POSITIONS = ["QB", "RB", "TE", "WR"]
+POSITIONS: list[str] = ["QB", "RB", "TE", "WR"]
 # TODO: Add support for P/K (need kicking/punting stats)
 
-TRAIN_YEARS = [2015]
-TEST_YEARS = [2016]
+TRAIN_YEARS: list[int] = [2015]
+TEST_YEARS: list[int] = [2016]
+
+DEBUG: bool = False
