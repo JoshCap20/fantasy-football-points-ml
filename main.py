a7390c7616e09125a5e7b7c37e9e67a8d5321dbc
Josh Caponigro <joshcap@email.unc.edu>
2024-09-03 00:17:11 -0400

    Add program overview in main

--- main.py
+++ main.py
@@ -2,6 +2,24 @@
 Entry point.
 
 Abstracts data cleaning, data processing, feature engineering, model training, and model evaluation.
+
+Data Loading and Cleaning
+Data is first loaded from saved file or scraped and saved locally as a local cache. 
+Player stats are combined with schedule stats based on season, week, and home/away team.
+
+Feature Engineering
+New features are created from existing data. 
+This includes new stats (i.e. passing_yards_per_attempt) and multiple averages for performance over different timeframes respectively.
+**Should toy around with different timeframes for rolling averages to see if it improves model performance and features.
+
+Model Training
+Models are trained by position (QB, RB, WR, TE) and saved to output filepath for later use.
+Model performance is evaluated (by RMSE for now) and saved to output filepath also.
+**Should add an ensemble model here to combine the individual models for a more accurate prediction.
+
+Analysis
+This was a standalone script originally to create helpful visualizations of performance.
+Now also outputs to output filepath on run.
 """
 
 from data_processing import load_data
